c03da637b0ed2124358806d5f5ff9e86f26b87e6
Julien Miotte <mike.perdide@gmail.com>
2012-10-02 22:50:24 +0200

    Using a better listofplugin.py (with debug).

--- designer_plugins/listofplugin.py
+++ designer_plugins/listofplugin.py
@@ -7,10 +7,29 @@ Copyright: 2011-2012 Feth Arezki <feth dot arezki at majerti dot fr>
 License: This file is released under the GPLv3
          http://www.gnu.org/licenses/gpl-3.0.txt
 """
+# Althought the following import isn't use here, if it's missing Qt4 Designer
+# won't aknowledge this as a plugin
 from PyQt4 import QtDesigner
-from designer import plugin
 
-from axis_widget import PyAxisWidget
+LOGFILE = "/tmp/generic_qt4_plugins.log"
 
-_MODULE_INCLUDE = 'pcoordsgui.widgets.axis_widget'
-AXIS_WIDGET = plugin(PyAxisWidget, "axis_widget", _MODULE_INCLUDE)
+# For debugging purposes, we're wrapping all of this file inside a try: except:
+import traceback
+try:
+    # Importing the plugin wrapper
+    from designer import plugin
+
+    # Below, list all the widget classes imports.
+    # If in launcher_designer.py the PYTHONPATH is set to the directory
+    # containing the widget classes, the import can be done like:
+    from axis_widget import PyAxisWidget
+
+    # The _include part must be thought through carefully, since it will be
+    # used in the generated (from the .ui) python file. It depends on where
+    # you'll keep the .ui of the widget that will include the plugin.
+    _MODULE_INCLUDE = 'pcoordsgui.widgets.axis_widget'
+    AXIS_WIDGET = plugin(PyAxisWidget, "axis_widget", _MODULE_INCLUDE)
+
+except Exception, e:
+    with open(LOGFILE, "a") as handle:
+        traceback.print_exc(file=handle)
